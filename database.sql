05c59a00fe7ba5866f2000701073c559d6be86a6
Natsinee Tanapornrat <101309777+ploynatsinee@users.noreply.github.com>
2022-10-04 09:18:16 +0700

    setup and add signup

---
+++ database.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE test
+
+CREATE TABLE users (
+    Id  INT PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(100) UNIQUE
+);
+
+CREATE TABLE todo (
+    todo description VARCHAR(255)
+);
---
+++ pages/api/signup.ts
@@ -0,0 +1,26 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const pool = require("../../db");
+import type { NextApiRequest, NextApiResponse } from "next";
+
+const createUser = async (req: NextApiRequest, res: NextApiResponse) => {
+  return new Promise<void>((resolve) => {
+    const { name, email } = req.body;
+    if (req.method === "POST") {
+      try {
+        const result = pool.query(
+          "INSERT INTO users (first_name, email) VALUES ($1, $2) RETURNING *",
+          [name, email],
+          (error, results) => {
+            res.status(201).send({ result });
+          }
+        );
+      } catch (error) {
+        res.status(400).send(error);
+        console.log(error);
+      }
+    }
+    return resolve();
+  });
+};
+
+export default createUser;
