767bfc646e39ddcef6738732286dadccd7874620
Natsinee Tanapornrat <101309777+ploynatsinee@users.noreply.github.com>
2022-10-05 23:55:40 +0700

    add signin and delete

--- pages/api/signin.ts
+++ pages/api/signin.ts
@@ -1,12 +1,35 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
+import pool from "../../db";
+import type { NextApiRequest, NextApiResponse } from "next";
 
 type ResponseData = {
-  message: string
-}
+  message: string;
+};
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<ResponseData>
-) {
-  res.status(200).json({ message: 'Hello from Next.js!' })
-}
+const signIn = async (req, res) => {
+  console.log("test");
+
+  const name = req.body.name;
+  const email = req.body.email;
+
+  const user = await pool.query(
+    `SELECT * FROM users WHERE first_name='${name}' AND email='${email}' `
+  );
+
+  if (req.method === "POST") {
+    if (user) {
+      try {
+        console.log("inside");
+        res.status(200).json({ message: "Signin success" });
+      } catch (error) {
+        res.status(400).send(error);
+        console.log(error);
+      }
+    }
+  } else {
+    res.status(200).json({ message: "Please sign up before you can sign in." });
+  }
+
+  console.log("exit");
+};
+
+export default signIn;
--- pages/api/todo.ts
+++ pages/api/todo.ts
@@ -3,24 +3,31 @@ import pool from "../../db";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 const createTodo = async (req, res) => {
-  return new Promise<void>((resolve) => {
-    const { todo } = req.body;
-    if (req.method === "POST") {
-      try {
-        pool.query(
-          "INSERT INTO todos (todo) VALUES ($1) RETURNING *",
-          [todo],
-          () => {
-            res.status(201).json({ message: 'create todo success' });
-          }
-        );
-      } catch (error) {
-        res.status(400).send(error);
-        console.log(error);
-      }
+  const { todo } = req.body;
+  const data = req.body.todo;
+
+  if (req.method === "POST") {
+    try {
+      pool.query("INSERT INTO todos (todo) VALUES ($1) RETURNING *", [todo]);
+      res.status(201).json({ message: "create todo success" });
+    } catch (error) {
+      res.status(400).send(error);
+      console.log(error);
     }
-    return resolve();
-  });
+  }
+
+  if (req.method === "DELETE") {
+    try {
+      const haveData = await pool.query(
+        `DELETE FROM todos WHERE todo='${data}' `
+      );
+      res.status(200).json(`delete data success ${haveData}`);
+      // res.status(200).json({ message: "delete todo success" });
+    } catch (error) {
+      res.status(400).send(error);
+      console.log(error);
+    }
+  }
 };
 
 export default createTodo;
