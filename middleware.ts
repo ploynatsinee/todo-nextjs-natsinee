2aa15cb688d5488ff3a4ffd4c12eb268af62614b
Natsinee Tanapornrat <101309777+ploynatsinee@users.noreply.github.com>
2022-10-06 13:23:12 +0700

    add code

--- middleware.ts
+++ middleware.ts
@@ -10,10 +10,10 @@ export function middleware(req: NextRequest, res: NextResponse) {
     }
     const response = NextResponse.next();
     const JWT = req.body;
-    const token = jwt.sign(JSON.stringify(JWT), "secret");
+    const token = jwt.sign(JSON.stringify(JWT), process.env.MY_SECRET);
     res.cookies.set("user_token", token);
     res.cookies.set("user_token", token, { path: "/api/signin" });
-
+   
     // Getting cookies from the request
     const cookie = req.cookies.get("user_token");
     console.log(cookie); 
@@ -34,3 +34,30 @@ export function middleware(req: NextRequest, res: NextResponse) {
 export const config = {
   matcher: "/api/signin",
 };
+
+// // middleware.ts
+// import { NextResponse } from 'next/server'
+// import type { NextRequest } from 'next/server'
+
+
+// export function middleware(request: NextRequest) {
+//   // Setting cookies on the response
+//   const response = NextResponse.next()
+//   response.cookies.set('token', 'fast')
+//   response.cookies.set('token', 'fast', { path: '/api/signin' })
+
+//   // Getting cookies from the request
+//   const cookie = request.cookies.get('vercel')
+//   console.log(cookie) // => 'fast'
+//   const allCookies = request.cookies.entries()
+//   console.log(allCookies) // => [{ key: 'vercel', value: 'fast' }]
+//   const { value, options } = response.cookies.getWithOptions('vercel')
+//   console.log(value) // => 'fast'
+//   console.log(options) // => { Path: '/test' }
+
+//   // Deleting cookies
+//   response.cookies.delete('vercel')
+//   response.cookies.clear()
+
+//   return response
+// }
