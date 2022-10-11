5d35cf671f58c810ec2a9e6c558e370113070a60
Natsinee Tanapornrat <101309777+ploynatsinee@users.noreply.github.com>
2022-10-11 13:36:26 +0700

    update sch (Unsuccessful)

--- database.sql
+++ database.sql
@@ -4,13 +4,13 @@ CREATE TABLE users (
     first_name VARCHAR(300),
     email VARCHAR(100) UNIQUE,
     password VARCHAR(1000),
-    PRIMARY KEY(first_name)
+    PRIMARY KEY (first_name)
 );
 
 CREATE TABLE todos (
+    name_user VARCHAR(300),
     todo VARCHAR(255),
     isSuccessful BOOLEAN,
-    first_name VARCHAR(300),
-    CONSTRAINT FK_users FOREIGN KEY(first_name)
-    REFERENCES users(first_name)
+    PRIMARY KEY (name_user),
+    CONSTRAINT fk_name_user FOREIGN KEY(name_user) REFERENCES users(first_name)
 );
