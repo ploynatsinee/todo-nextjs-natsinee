24cf3338b14fea4e1308129c9b3b1afc24eefe63
Natsinee Tanapornrat <101309777+ploynatsinee@users.noreply.github.com>
2022-10-11 09:45:03 +0700

    add pgcrypto and try to change sch to do st

--- database.sql
+++ database.sql
@@ -1,10 +1,16 @@
+CREATE EXTENSION pgcrypto;
+
 CREATE TABLE users (
     first_name VARCHAR(300),
     email VARCHAR(100) UNIQUE,
-    password VARCHAR(1000)
+    password VARCHAR(1000),
+    PRIMARY KEY(first_name)
 );
 
 CREATE TABLE todos (
     todo VARCHAR(255),
-    isSuccessful BOOLEAN
+    isSuccessful BOOLEAN,
+    first_name VARCHAR(300),
+    CONSTRAINT FK_users FOREIGN KEY(first_name)
+    REFERENCES users(first_name)
 );
