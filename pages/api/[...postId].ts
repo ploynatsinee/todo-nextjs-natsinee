c0cc60d70a5227645d4d29a594a7ba57f9b34945
Natsinee Tanapornrat <101309777+ploynatsinee@users.noreply.github.com>
2022-10-11 13:57:18 +0700

    update query param

--- pages/api/[...postId].ts
+++ pages/api/[...postId].ts
@@ -1,4 +1,4 @@
 export default function handler(req, res) {
-  const { name,todo } = req.query["name"];
-  res.status(200).json({ name: name, todos: todo });
+  const { name } = req.query["name"];
+  res.status(200).json({ name: name});
 }
