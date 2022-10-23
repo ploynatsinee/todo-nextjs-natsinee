7a1b9aeac5589c5bf9cecf134b7c44e3623bf601
Natsinee Tanapornrat <101309777+ploynatsinee@users.noreply.github.com>
2022-10-23 08:01:12 +0700

    can click and set verified to true

--- pages/api/verify.ts
+++ pages/api/verify.ts
@@ -1,10 +1,30 @@
-// method GET
+import pool from "../../db";
 
 const verifyUser = async (req, res) => {
+  const verifyTokens = req.query.verifyToken;
+
   try {
+    const user = await pool.query(
+      `SELECT user_token FROM users WHERE user_token=$1`,
+      [verifyTokens],
+      (err, results) => {
+        if (err) {
+          throw err;
+        }
 
-    // SELECT from DB if token = req.param SET verify to 'true'
+        if (results.rows.length < 1) {
+          res.status(202).send("Token not found, Please resend token");
+          return;
+        }
 
+        if (results.rows.length) {
+          pool.query(`UPDATE users SET verified = true WHERE user_token= $1`, [
+            verifyTokens,
+          ]);
+          res.status(200).send("verified user success");
+        }
+      }
+    );
   } catch (error) {
     console.log(error);
   }
