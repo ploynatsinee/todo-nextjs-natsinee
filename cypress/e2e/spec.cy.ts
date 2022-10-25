5941185c40026b059c81350c4537e8d177785386
Natsinee Tanapornrat <101309777+ploynatsinee@users.noreply.github.com>
2022-10-25 16:43:24 +0700

    still cannot find name cy

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,7 +1,7 @@
 describe('navigate', () => {
   it('should navigate to the sign up page', () => {
-    // cy.visit('http://localhost:3000/')
-    cy.visit('https://nextjs.org/docs/testing#cypress')
+    cy.visit('http://localhost:3000/')
+    // cy.visit('https://nextjs.org/docs/testing#cypress')
 
   })
 })
