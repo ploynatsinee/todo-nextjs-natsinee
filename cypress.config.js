7025d77a3a3732be613f7334441a10230565d356
Natsinee Tanapornrat <101309777+ploynatsinee@users.noreply.github.com>
2022-10-26 10:58:55 +0700

    add specPattern

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,10 @@ module.exports = defineConfig({
   chromeWebSecurity: false,
   e2e: {
     baseUrl: "http://web:3000",
+    specPattern: [
+      "cypress/e2e/spec.cy.ts",
+      "cypress/e2e/email.cy.ts"
+    ],
   },
   // output: {
   //   filename: "index.js",
