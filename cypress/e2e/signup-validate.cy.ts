37d211fa934c735c07c77a8ef2afeccedb0389e0
Natsinee Tanapornrat <101309777+ploynatsinee@users.noreply.github.com>
2022-10-26 12:19:03 +0700

    mock up test sign up validate

--- cypress/e2e/signup-validate.cy.ts
+++ cypress/e2e/signup-validate.cy.ts
@@ -1,6 +1,22 @@
 describe("sign up integration test", () => {
-    it("should navigate to the sign up page", () => {
-      cy.visit("http://localhost:3000/");
-    });
+  it("should navigate to the sign up page", () => {
+    cy.visit("http://localhost:3000/");
+  });
 
-})
+  it("no input", () => {
+    cy.get(".MuiButtonBase-root").click();
+    cy.on("window:alert", (str) => {
+      expect(str).to.equal("Please fill out the information completely.");
+    });
+  });
+
+  it("incomplete input", () => {});
+
+  it("name > 25 character", () => {});
+
+  it("password < 7 character", () => {});
+
+  it("password > 25 character", () => {});
+
+
+});
