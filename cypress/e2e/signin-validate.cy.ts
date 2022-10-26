e980512993f56a9276aef957d9dd4f9b864a9190
Natsinee Tanapornrat <101309777+ploynatsinee@users.noreply.github.com>
2022-10-26 13:23:07 +0700

    signin validate til dont have users

--- cypress/e2e/signin-validate.cy.ts
+++ cypress/e2e/signin-validate.cy.ts
@@ -1,18 +1,27 @@
 describe("sign in integration test", () => {
-    it("should navigate to the sign in page", () => {
-      cy.visit("http://localhost:3000/signin");
-    });
-  
-    it("no input", () => {
-    //   cy.get(".MuiButtonBase-root").click();
-    //   cy.on("window:alert", (str) => {
-    //     expect(str).to.equal("Please fill out the information completely.");
-    //   });
-    });
-  
-    it("don't have user", () => {});
-  
-    it("user not validate email", () => {});
-  
+  it("should navigate to the sign in page", () => {
+    cy.visit("http://localhost:3000/signin");
   });
-  
+
+  it("no input", () => {
+    cy.get(".MuiButton-root").click();
+    cy.on("window:alert", (str) => {
+      expect(str).to.equal("Please fill out the information completely.");
+    });
+  });
+
+  it("don't have user", () => {
+    const email = "nouser@gmail.com";
+    const password = "nousernouser";
+    cy.get("#email").type(email).should("have.value", email);
+    cy.get("#password").type(password).should("have.value", password);
+    cy.get(".MuiButton-root").click();
+    cy.on("window:alert", (str) => {
+      expect(str).to.equal(
+        "User not found, Please recheck your email or password"
+      );
+    });
+  });
+
+  // it("user not validate email", () => {});
+});
